927210fd0ccd9f68c324288630757d3e058f4ad1
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-03-21 03:46:16 +0100

    shindaiwa

---
+++ tempoverde/tempoverde/spiders/shindaiwa.py
@@ -0,0 +1,70 @@
+from email.mime import image
+from numpy import product
+import scrapy
+from tempoverde.items import ImgItem
+
+
+class ShindaiwaSpider(scrapy.Spider):
+    name = 'shindaiwa'
+    allowed_domains = ['shindaiwa-italia.it']
+    start_urls = [
+                'https://www.shindaiwa-italia.it/prodotti/motoseghe/motoseghe-potatura',
+                'https://www.shindaiwa-italia.it/prodotti/motoseghe/motoseghe-multifunzione',
+                'https://www.shindaiwa-italia.it/prodotti/motoseghe/motoseghe-forestali',
+                'https://www.shindaiwa-italia.it/prodotti/potatori/potatori-telescopici',
+                'https://www.shindaiwa-italia.it/prodotti/soffiatori/soffiatori-aspiratori',
+                'https://www.shindaiwa-italia.it/prodotti/soffiatori/soffiatori-a-mano',
+                'https://www.shindaiwa-italia.it/prodotti/soffiatori/soffiatori-a-zaino',
+                'https://www.shindaiwa-italia.it/prodotti/tosasiepi/tosasiepi-ad-asta',
+                'https://www.shindaiwa-italia.it/prodotti/tosasiepi/tosasiepi-a-doppia-lama',
+                'https://www.shindaiwa-italia.it/prodotti/tosasiepi/tosasiepi-a-lama-singola',
+                'https://www.shindaiwa-italia.it/prodotti/decespugliatori/decespugliatori-a-zaino',
+                'https://www.shindaiwa-italia.it/prodotti/decespugliatori/decespugliatori-standard',
+                'https://www.shindaiwa-italia.it/prodotti/altre-macchine/macchina-multifunzione',
+                'https://www.shindaiwa-italia.it/prodotti/altre-macchine/mototroncatrice',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/lame-e-catene',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/testine-a-filo',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/filo-in-nylon',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/dischi-di-taglio',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/altri-accessori',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/protettivo-catene',
+                'https://www.shindaiwa-italia.it/system/product_category/image/88/box_olio-mix.jpg',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/abbigliamento-antitaglio',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/abbigliamento-protettivo',
+                'https://www.shindaiwa-italia.it/prodotti/accessori/abbigliamento-casual',
+                ]
+
+    custom_settings = {
+        'IMAGES_STORE': './images/shindaiwa',
+        'FEED_URI' : "./shindaiwa.xlsx"
+    }
+
+    def parse(self, response):
+        for link in response.css('section#product-list a.c-item::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_products)
+
+    def parse_products(self, response):
+        descrizione = response.css('h1::text').get().strip() if response.css('h1::text').get() is not None else None
+        #sottocategoria = response.css('li.expanded.active-trail a:first-child::text').get().strip() if response.css('li.expanded.active-trail a:first-child::text').get() is not None else None
+        price = response.css('h2.c-product__price::text').get().strip() if response.css('h2.c-product__price::text').get() is not None else None
+        description = response.css('div#long-description::text').get().strip() if response.css('div.content.product-page div.description::text').get() is not None else None
+        details = response.xpath('//*[@class="c-product__data"]/table//tr').xpath('normalize-space()').getall() if response.css('div#description ul li').get() is not None else None
+        if details is not []:
+            note = "\n".join(details)
+            yield {'Note': note,}
+
+        img = ImgItem()
+        img['image_urls'] = [response.urljoin(response.css('ul.c-slider__wrap  img::attr(src)').get())]
+        img['image_name'] = descrizione.replace(" ","-")
+
+        yield {
+           # 'Sottocategoria': sottocategoria,
+            'Descrizione': descrizione,
+            'Listino 4 (ivato)': price,
+            'Produttore': "Shindaiwa",
+            'Cod. Fornitore': "0000",
+            'Categoria': "Macchine",
+            'Internet': response.url,
+        }
+
+        yield img

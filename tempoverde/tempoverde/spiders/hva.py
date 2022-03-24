a9dc5c04f296f8e1fec41e9f8f4cedbb2344e7ed
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-03-24 12:11:27 +0100

    hva login

--- tempoverde/tempoverde/spiders/hva.py
+++ tempoverde/tempoverde/spiders/hva.py
@@ -1,15 +1,15 @@
 import scrapy
 from scrapy.http import FormRequest
 
-def start_requests(self):
-   return [
-      FormRequest("INSERT URL", formdata={"user":"D1037749",
-           "pass":"iU521hDE"}, callback=self.parse)]
-
 class HvaSpider(scrapy.Spider):
     name = 'hva'
     allowed_domains = ['supportsites.husqvarnagroup.com']
-    start_urls = ['http://supportsites.husqvarnagroup.com/']
+    start_urls = ['https://supportsites.husqvarnagroup.com/it']
 
-    def parse(self, response):
-        pass
+    def parse(self,response):
+        csrf_token = response.xpath('//*[@name="csrf_token"]/@value').extract_first()
+        print(csrf_token)
+        yield FormRequest.from_response(response, formdata={'csrf_token': csrf_token, 'username':'D1037749', 'password': 'iU521hDE'}, callback=self.parse_after_login)
+
+    def parse_after_login(self,response):
+        print(response.xpath('.//div[@class = "col-md-4"]/p/a/text()'))

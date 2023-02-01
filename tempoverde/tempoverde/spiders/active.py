34ff5d94935709d3ea9526f94a871a9605f53a8e
Fabio Pegorari <fabio.pegorari@gmail.com>
2023-02-02 00:39:40 +0100

    active

--- tempoverde/tempoverde/spiders/active.py
+++ tempoverde/tempoverde/spiders/active.py
@@ -17,9 +17,10 @@ class ActiveSpider(scrapy.Spider):
         'https://www.active-srl.com/it/catalogo/generatori/generatori-di-corrente',
         'https://www.active-srl.com/it/catalogo/trivelle',
         'https://www.active-srl.com/it/catalogo/decespugliatori/professionali-zaino-multifunzione',
-        'https://www.active-srl.com/it/catalogo/rasaerba/macchine-specifiche-mulching',
         'https://www.active-srl.com/it/catalogo/arieggiatore',
         'https://www.active-srl.com/it/catalogo/motozappe',
+        'https://www.active-srl.com/it/catalogo/accessori/testine-decespugliatori-batti-e-vai',
+        'https://www.active-srl.com/it/catalogo/power-track',
         ]
 
     custom_settings = {
@@ -31,7 +32,8 @@ class ActiveSpider(scrapy.Spider):
     df = pd.read_excel(SpreadsheetSource)
 
     def parse(self, response):
-        for link in response.css('div.product a.see-details-hover::attr(href)'):
+        print(response)
+        for link in response.css('a.see-details-hover::attr(href)'):
             yield response.follow(link.get(), callback=self.parse_products)
 
     def parse_products(self, response):
@@ -42,14 +44,14 @@ class ActiveSpider(scrapy.Spider):
         note = "\n".join(details)
         img_name = descrizione.replace(" ","-")
         img_src = [response.urljoin(response.css('article.product-page div.image-box div.general-img img:first-child::attr(src)').get())]
-        prod_cod = self.df.loc[self.df["Descrizione"] == descrizione.upper(), "Codice"].item()
-        prod_price = self.df.loc[self.df["Descrizione"] == descrizione.upper(), "Pubblico +IVA"].item()
+      #  prod_cod = self.df.loc[self.df["Descrizione"] == descrizione.upper(), "Codice"].item()
+      #  prod_price = self.df.loc[self.df["Descrizione"] == descrizione.upper(), "Pubblico +IVA"].item()
 
         yield {
-            'Cod.': prod_cod,
+         #   'Cod.': prod_cod,
             'Sottocategoria': sottocategoria,
             'Descrizione': descrizione,
-            'Listino 4 (ivato)': prod_price,
+          #  'Listino 4 (ivato)': prod_price,
             'Note': note,
             'Produttore': "Active srl",
             'Cod. Fornitore': "0006",
